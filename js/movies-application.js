6656ddccfc576ed24640afd19fd81177620a7ab5
Casper <clydenberg@gmail.com>
2023-12-04 10:10:06 -0600

    functions

--- js/movies-application.js
+++ js/movies-application.js
@@ -47,6 +47,43 @@ const postMovie = async (movie) => {
         return data;
 }
 
+const patchMovie = async (movie) => {
+
+            const updatedMovie = {
+                ...movie,
+            }
+
+            const body = JSON.stringify(updatedMovie);
+
+            const url = `http://localhost:3000/movies/${movie.id}`;
+            const options = {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: body
+            }
+            const response = await fetch(url, options);
+            const data = await response.json();
+            return data;
+}
+
+const deleteMovie = async (id) => {
+    const url = `http://localhost:3000/movies/${id}`;
+    const options = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+}
+
+
+
+
 (async()=>{
 
 
